eb34b8820637b9246479b8085143d7db94952bdf
Nisit007 <53615956+Nisit007@users.noreply.github.com>
2020-02-28 10:52:24 +0530

    Create Multiprocessing.py

---
+++ Multiprocessing.py
@@ -0,0 +1,59 @@
+import multiprocessing
+print("Number of cpu : ", multiprocessing.cpu_count())
+###############################################################
+# from multiprocessing import Process
+#
+#
+# def print_func(continent='Asia'):
+#     print('The name of continent is : ', continent)
+#
+# if __name__ == "__main__":  # confirms that the code is under main function
+#     names = ['America', 'Europe', 'Africa']
+#     procs = []
+#     proc = Process(target=print_func)  # instantiating without any argument
+#     procs.append(proc)
+#     proc.start()
+#
+#     # instantiating process with arguments
+#     for name in names:
+#         # print(name)
+#         proc = Process(target=print_func, args=(name,))
+#         procs.append(proc)
+#         proc.start()
+#
+#     # complete the processes
+#     for proc in procs:
+#         proc.join()
+########################################################################
+import multiprocessing
+def print_cube(num):
+    """
+    function to print cube of given num 
+    """
+    print("Cube: {}".format(num * num * num))
+
+
+def print_square(num):
+    """
+    function to print square of given num
+    """
+    print("Square: {}".format(num * num))
+
+
+if __name__ == "__main__":
+    # creating processes
+    p1 = multiprocessing.Process(target=print_square, args=(10,))
+    p2 = multiprocessing.Process(target=print_cube, args=(10,))
+
+    # starting process 1
+    p1.start()
+    # starting process 2
+    p2.start()
+
+    # wait until process 1 is finished
+    p1.join()
+    # wait until process 2 is finished
+    p2.join()
+
+    # both processes finished
+    print("Done!")
